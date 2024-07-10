3c1b6b53332e7a14044c66cbbf77b9586303a8f7
sean wong <wongs28@uw.edu>
2024-07-10 15:21:59 -0700

    commit 2: comments added

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from matplotlib import pyplot as plt, colors
 # from matplotlib import cm
 # import skimage as ski
 
+# convert piezo voltage data into an array
 volt_data = np.loadtxt("image_data.txt", dtype=float)
 print(volt_data)
 print(np.size(volt_data))
-
+# convert piezo voltage to height displacement
+# *100 because .Normalize() normalizes between 0 and 1
 height_data = colors.Normalize()(volt_data) * 100
 print(height_data)
-
+# create colormap of data
 x, y = np.mgrid[0:20:0.2, 0:20:0.2]
 V = volt_data
 z = height_data
