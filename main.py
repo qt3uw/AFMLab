5468db5db667c57b7c1f8c7d14a300661c410fc7
sean wong <wongs28@uw.edu>
2024-07-15 16:37:51 -0700

    commit 3: get_afm_data

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import numpy as np
 from matplotlib import pyplot as plt, colors
-# from matplotlib import cm
-# import skimage as ski
+from pathlib import Path
 
-# convert piezo voltage data into an array
-volt_data = np.loadtxt("image_data.txt", dtype=float)
-print(volt_data)
-print(np.size(volt_data))
-# convert piezo voltage to height displacement
-# *100 because .Normalize() normalizes between 0 and 1
-height_data = colors.Normalize()(volt_data) * 100
-print(height_data)
-# create colormap of data
-x, y = np.mgrid[0:20:0.2, 0:20:0.2]
-V = volt_data
-z = height_data
-plt.pcolormesh(x, y, z, cmap='Greys')
-plt.title("AFM Scan")
-plt.xlabel('x pos [microns]')
-plt.ylabel('y pos [microns]')
-# plt.colorbar(label="Z piezo voltage [V]")
-plt.colorbar(label="Z piezo displacement [nm]")
-plt.show()
 
+def get_afm_data(filepath):
+    volt_data = np.loadtxt(filepath, dtype=float, delimiter=';')
+    keys = ["name", "scan mode", "strain gauge", "resolution", "scan speed"]
+    get_string = str(filepath)
+    np.loadtxt(get_string, dtype=str, delimiter='_')
+    # scan_params = {name: ; mode: ; resolution: }
+
+def create_image(filepath):
+    # convert piezo voltage data into an array
+    # filepath = Path(r"C:\Users\QT3\Documents\EDUAFM Data\TestSample_ConstantForce_StrainGauge_100px_100pps.csv")
+    volt_data = np.loadtxt(filepath, dtype=float, delimiter=';')
+    print(volt_data)
+    print(np.size(volt_data))
+    # convert piezo voltage to height displacement
+    # *100 because .Normalize() normalizes between 0 and 1
+    # height_data = colors.Normalize()(volt_data) * 100
+    # print(height_data)
+    # create colormap of data
+    x, y = np.mgrid[0:20:0.2, 0:20:0.2]
+    v = volt_data
+    # z = height_data
+    plt.pcolormesh(x, y, v, cmap='Greys')
+    plt.title("AFM Scan")
+    plt.xlabel('x pos [microns]')
+    plt.ylabel('y pos [microns]')
+    plt.colorbar(label="Z piezo voltage [V]")
+    # plt.colorbar(label="Z piezo displacement [nm]")
+    plt.show()
