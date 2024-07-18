9493e6e9bf46fe11c981e6273556bdb04d281bdb
Sean Wong <wongs28@uw.edu>
2024-07-18 13:34:19 -0700

    create tuple with numpy array and scan parameters

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ DATA_DIR = Path(r'C:\Users\QT3\Documents\EDUAFM Data')
 
 
 def get_afm_data(folder_path):
-    # Initialize an empty list to store data frames
+    # Initialize an empty list to store numpy arrays
     array_list = []
+    str_list = []
 
     # Iterate over all files in the directory
     for file_name in os.listdir(folder_path):
@@ -21,10 +22,20 @@ def get_afm_data(folder_path):
                 df = pd.read_csv(f, delimiter=';', header=None)
                 array = df.to_numpy()[:, :-1].astype(float)
 
+            # Convert file_name into list of identifiers
+            if str(file_name)[:-4].endswith(')'):
+                lists = str(file_name)[:-8].split('_')
+            else:
+                lists = str(file_name)[:-4].split('_')
+
             # Append the numpy array to the list
             array_list.append(array)
+            str_list.append(lists)
 
-    return array_list
+            # Merge into list of tuples
+    tuple_list = list(zip(str_list, array_list))
+
+    return tuple_list
 
 
 def create_image(data):
@@ -40,4 +51,4 @@ def create_image(data):
 if __name__ == '__main__':
     AFMdata = get_afm_data(DATA_DIR)
     print(AFMdata[0])
-    create_image(AFMdata[0])
+    create_image(AFMdata[0][1])
