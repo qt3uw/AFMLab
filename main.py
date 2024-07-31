8c34cd5baa41d5173645c95cd4dcdde367c374fc
Sean Wong <wongs28@uw.edu>
2024-07-31 14:13:37 -0700

    denoise function to help define step region

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import numpy as np
-from matplotlib import pyplot as plt, colors
+from matplotlib import pyplot as plt
 from pathlib import Path
 import os
 import pandas as pd
-from skimage import filters as flt
-from skimage.morphology import disk
 from skimage.exposure import rescale_intensity
+from scipy.signal import savgol_filter
 
 DATA_DIR = Path(r'C:\Users\QT3\Documents\EDUAFM\Scans')
 
@@ -42,6 +41,16 @@ def get_afm_data(folder_path):
     return tuple_list
 
 
+def print_data(data_list, labels):
+    if isinstance(labels, list):
+        for data, label in zip(data_list, labels):
+            neat_format = '{}'.format(label) + '\n' + '\n'.join(str(item) for item in data)
+            print(neat_format)
+    else:
+        neat_format = '\n'.join(str(item) for item in data_list)
+        print(neat_format)
+
+
 def create_image(data_info, data):
     # Get width of scan from data_info
     width = float(data_info[data_info.index("zoom") + 1][:-6])
@@ -63,9 +72,9 @@ def create_image(data_info, data):
 
 def create_plot(num_plots, plot_type, x_data, y_data, title, x_label, y_label, leg_label, leg_title):
     fig, ax = plt.subplots(num_plots, 1)
-    # Create multiple plots or overlay data onto one plot
+    # One plot case
     if num_plots == 1:
-        # plot multiple data sets or one data set
+        # Multiple data sets subcase
         if isinstance(leg_label, list):
             for x, y, label in zip(x_data, y_data, leg_label):
                 # create array for x_data if value is floating point
@@ -79,7 +88,8 @@ def create_plot(num_plots, plot_type, x_data, y_data, title, x_label, y_label, l
                         ax.scatter(x, y, label=label)
                     elif plot_type == 'line':
                         ax.plot(x, y, label=label)
-                plt.legend(title=leg_title)
+            plt.legend(title=leg_title)
+        # One data set subcase
         else:
             if plot_type == 'scatter':
                 ax.scatter(x_data, y_data)
@@ -89,12 +99,30 @@ def create_plot(num_plots, plot_type, x_data, y_data, title, x_label, y_label, l
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
         ax.grid(True)
+    # Multiple plots case
     else:
         for i in range(num_plots):
-            if plot_type == 'scatter':
-                ax[i].scatter(x_data, y_data[i])
-            elif plot_type == 'line':
-                ax[i].plot(x_data, y_data[i])
+            # Multiple data sets per plot subcase
+            if isinstance(leg_label, list):
+                for x, y, label in zip(x_data, y_data[i], leg_label):
+                    # create array for x_data if value is floating point
+                    if isinstance(x, float):
+                        if plot_type == 'scatter':
+                            ax[i].scatter(np.linspace(0, x, len(y)), y, label=label)
+                        elif plot_type == 'line':
+                            ax[i].plot(np.linspace(0, x, len(y)), y, label=label)
+                    else:
+                        if plot_type == 'scatter':
+                            ax[i].scatter(x, y, label=label)
+                        elif plot_type == 'line':
+                            ax[i].plot(x, y, label=label)
+                    ax[i].legend(title=leg_title)
+            # One data set per plot subcase
+            else:
+                if plot_type == 'scatter':
+                    ax[i].scatter(x_data, y_data[i])
+                elif plot_type == 'line':
+                    ax[i].plot(x_data, y_data[i])
             fig.suptitle(title)
             ax[i].set_xlabel(x_label)
             ax[i].set_ylabel(y_label[i])
@@ -117,24 +145,26 @@ def volt_to_height(volt_data):
     return heights
 
 
-def find_edges(images, dx, dy):
+def find_edges(images):
     # Initialize list of arrays for sliced data
     slices = []
     widths = []
 
     # Loop through all images
     for image in images:
+        image_info = image[0]
+        image_data = image[1]
         # Only execute find_edge for zoomed images
-        if "zoom" not in image[0]:
+        if "zoom" not in image_info:
             return
         else:
             # Get width of scan from image_info
-            width = float(image[0][image[0].index("zoom") + 1][:-6])
-            ppm = np.shape(image[1])[0] / width
+            width = float(image_info[image_info.index("zoom") + 1][:-6])
+            # ppm = np.shape(image_data)[0] / width
 
             # sliced horizontal and vertical scans in midpoints of the image
-            z_x = image[1][int(np.shape(image[1])[0] / 2 + (dx * ppm)), :]
-            # z_y = image[1][:, int(np.shape(image[1])[0] / 2 + (dy * ppm))]
+            z_x = image_data[int(np.shape(image_data)[0] / 2), :]
+            # z_y = image[1][:, int(np.shape(image[1])[0] / 2)]
 
             # Add array and width data to lists
             slices.append(z_x)
@@ -150,7 +180,7 @@ def get_step_width(scan_width, scan_data):
     for width, data in zip(scan_width, scan_data):
         ppm = len(data) / float(width)
         # upper and lower bounds for isolating the step region
-        upper = 0.8 * np.max(data)
+        upper = 0.9 * np.max(data)
         lower = 0.2 * np.max(data)
         indices = np.where(np.logical_and(data < upper, data > lower))[0]
         step_width = abs(indices[-1] - indices[0]) / ppm
@@ -179,33 +209,57 @@ def get_noise(scan_data):
     return peaks, rms
 
 
+def denoise(scan_data):
+    smoothed = []
+    for data in scan_data:
+        smooth = savgol_filter(data, 8, 1)
+        smoothed.append(smooth)
+
+    return smoothed
+
+
 def tilt_correction(scan_width, scan_data):
+    # Initialize list for tilt corrected data
     sub_arrays = []
+    sub_arrays1 = []
     for width, data in zip(scan_width, scan_data):
         ppm = len(data) / float(width)
+        # upper and lower bounds for isolating flat region
+        upper = 0.9 * np.max(data)
+        lower = 0.1 * np.max(data)
         # Check if step is falling or rising
         if np.argmax(data) < np.argmin(data):
-            # y = np.concatenate((data[:np.argmax(data)], data[np.argmin(data):len(data)-1]))
-            y = data[:np.argmax(data)]
-            x = np.linspace(0, np.argmax(data) / ppm, len(y))
+            linear_region = np.where(data > upper)[0][-1]-10
+            y = data[:linear_region]
+            y1 = data[:np.argmax(data)]
+            x = np.linspace(0, linear_region / ppm, len(y))
+            x1 = np.linspace(0, np.argmax(data) / ppm, len(y1))
         else:
-            y = data[:np.argmin(data)]
-            x = np.linspace(0, np.argmin(data) / ppm, len(y))
+            linear_region = np.where(data < lower)[0][-1]-10
+            y = data[:linear_region]
+            y1 = data[:np.argmin(data)]
+            x = np.linspace(0, linear_region / ppm, len(y))
+            x1 = np.linspace(0, np.argmin(data) / ppm, len(y1))
         # Fit the baseline from the original data
         fit = np.polyfit(x, y, 1)
+        fit1 = np.polyfit(x1, y1, 1)
         linear_baseline = np.poly1d(fit)
+        linear_baseline1 = np.poly1d(fit1)
 
-        # Subtract the linear baseline from all data points
+        # Subtract the linear baseline from all data points and rescale
         sub_data = data - linear_baseline(np.linspace(0, width, len(data)))
+        sub_data1 = data - linear_baseline1(np.linspace(0, width, len(data)))
         scaled = rescale_intensity(sub_data, out_range=(0, 114))
+        scaled1 = rescale_intensity(sub_data1, out_range=(0, 114))
         sub_arrays.append(scaled)
+        sub_arrays1.append(scaled1)
 
-    return sub_arrays
+    return sub_arrays, sub_arrays1
 
 
 if __name__ == '__main__':
     AFMdata = get_afm_data(DATA_DIR)
-    includes = ["zoom", "backward"]
+    includes = ['zoom', 'backward']
     excludes = ['3.5micron', '3.7micron', '3.9micron']
     subset = []
     for scan in AFMdata:
@@ -214,12 +268,13 @@ if __name__ == '__main__':
             # print('\n'.join(str(item) for item in tup))
             subset.append(scan)
     # create_image(subset[0], subset[1])
+    # print_data(subset, None)
     height_data = volt_to_height(subset)
-    edges = find_edges(subset, 0, 0)
+    edges = find_edges(subset)
     scan_widths = edges[0]
-    # print('\n'.join(str(item) for item in edges))
+    # print_data(edges, ['Scan Widths:', 'Scan Data:'])
     height_slice = volt_to_height(edges[1])
-    # print('\n'.join(str(item) for item in height_slice))
+
     speeds = []
     for sub in subset:
         speeds.append(int(sub[0][4][5:-3]))
@@ -233,18 +288,18 @@ if __name__ == '__main__':
                 speeds,
                 'scanning speed [pps]')
     tilt_corrected = tilt_correction(scan_widths, height_slice)
-    create_plot(1,
+    denoised = denoise(tilt_corrected[0])
+    create_plot(2,
                 'line',
                 scan_widths,
-                tilt_corrected,
-                'Tilt Corrected',
+                [tilt_corrected[0], denoised],
+                'Tilt Corrected and Denoised',
                 'x pos [microns]',
-                'height [nm]',
+                ['height [nm]', 'height [nm]'],
                 speeds,
                 'scanning speed [pps]')
-    flat = get_noise(tilt_corrected)
-    # print('\n'.join(str(item) for item in flat))
-    print(flat)
+    flat = get_noise(tilt_corrected[0])
+    # print_data(flat, ['Pk to Pk:', 'RMS'])
     create_plot(2,
                 'scatter',
                 speeds,
@@ -254,8 +309,8 @@ if __name__ == '__main__':
                 ['Pk to Pk [nm]', 'RMS [nm]'],
                 None,
                 None)
-    steps = get_step_width(scan_widths, tilt_corrected)
-    print('\n'.join(str(item) for item in steps))
+    steps = get_step_width(scan_widths, denoised)
+    # print('\n'.join(str(item) for item in steps))
     create_plot(1,
                 'scatter',
                 speeds,
